827ee155daacc564f30fbb8a1f88edbb91b584e5
sharoncr98 <chrysler2102@gmail.com>
2025-09-25 20:04:43 -0400

    Adding toUpperCase
    made the input from the user convert to upper case just in case.

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -4,6 +4,8 @@ var prompt = require('prompt');
 //starting the process
 prompt.start();
 //using prompt to get the users input and store it as userSelection as instructed
+//i have made the input from the user convert to upper case just in case.
 prompt.get([userSelection], function (err, result) {
-
+    let userSelection = result.userSelection.toUpperCase();
 });
+

a5fb7ea65f8855dbd41d4a025fd00710bca65e4f
sharoncr98 <chrysler2102@gmail.com>
2025-09-25 20:00:18 -0400

    setting up the basic prompt package
    setting up the basic prompt package

---
+++ LAB02/lab2.js
@@ -0,0 +1,9 @@
+// As described in: https://www.npmjs.com/package/prompt - Getting the Basic Prompt to work
+
+var prompt = require('prompt');
+//starting the process
+prompt.start();
+//using prompt to get the users input and store it as userSelection as instructed
+prompt.get([userSelection], function (err, result) {
+
+});

0c0377de9a0881d9fbaf95d4c365e2d9a18b6af0
sharoncr98 <chrysler2102@gmail.com>
2025-10-10 18:40:42 -0400

    Created the required HBS files and the routers
    About, home, contact and projects hbs files created. Also defined the routers in the index.js file.

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//home page
+router.get("/", (req, res) => {
+  res.render("home", { title: "Home Page"})
 });
+//about page
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About Page"})
+});
+//projects page
+router.get("/projects", (req, res) => {
+  res.render("projects", { title: "Projects Page"})
+});
+//contact page
+router.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact Page"})
+});
+
+
+
 
 module.exports = router;
